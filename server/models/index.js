30f1deae2d8327deddcf5c402702ab8d84e0f37e
Brad Titus <bltitus@me.com>
2021-06-15 23:55:03 -0400

    finalize ElevenJournal front end

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
-const userModel = require("./user");
+const UserModel = require("./user");
 const JournalModel = require("./journal");
 
 module.exports = { 
     UserModel,
     JournalModel
- };
+};
